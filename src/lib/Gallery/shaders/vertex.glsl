39fb5fc0d59c7645c918c82a0866124ad119a24d
Ethan Olsen <131eolsen@gmail.com>
2024-03-16 11:08:46 -0700

    chore(gallery): shaders

---
+++ src/lib/Gallery/shaders/vertex.glsl
@@ -0,0 +1,18 @@
+precision highp float;
+
+attribute vec3 position;
+attribute vec2 uv;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+varying vec2 vUv;
+
+void main()
+{
+	vUv = uv;
+
+	vec3 p = position;
+
+	gl_Position = projectionMatrix * modelViewMatrix * vec4(p, 1.0);
+}
